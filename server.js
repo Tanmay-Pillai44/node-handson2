ce72d9332af6fefe488f08f8292e5003cd57269e
Tanmay_Pillai <omtej4499@gmail.com>
2022-05-18 22:57:34 +0530

    Uploading

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express"); // it will load the express package in our app
+const path = require("path");
+
+const port = 9000;
+
+// Creating app Object
+const app = express();
+
+// Route Creation
+
+app.get("/api/main", (req, res, next) => {
+
+    res.send("Hello World"); // text response
+    res.send("<h1>Hello World</h1>") // html response
+    res.send({
+        name: 'Tanmay',
+        age: 22
+    }); // json response
+
+    const filePath=path.join(__dirname+"/views/file.html") // the absolute path of my file
+    res.sendFile(filePath); // file response
+})
+
+// listening to the port
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`)
+})
